10224e55fbc0b84cb1cee131e23c2f7a465f34bd
Serg-Nikitin <nikitin879@yandex.ru>
2024-06-03 14:52:24 +0500

    add bean passwordEncoder and add annotation above RefreshTokenIfUserExistsFunction

--- src/main/java/ru/nikitin/jwt/service/function/RefreshTokenIfUserExistsFunction.java
+++ src/main/java/ru/nikitin/jwt/service/function/RefreshTokenIfUserExistsFunction.java
@@ -1,9 +1,11 @@
 package ru.nikitin.jwt.service.function;
 
+import org.springframework.stereotype.Service;
 import ru.nikitin.jwt.model.User;
 
 import java.util.function.Function;
 
+@Service
 public class RefreshTokenIfUserExistsFunction implements Function<String, User> {
     @Override
     public User apply(String s) {
